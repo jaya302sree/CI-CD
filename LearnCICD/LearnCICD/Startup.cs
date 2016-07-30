0dd0bfd9fe55fce3bfcafa3e29d187f4e80e8bb9
jayasree ammanabrolu <jayasreeammanabrolu@live.com>
2016-07-29 22:43:33 -0700

    InitialFiles

---
+++ LearnCICD/LearnCICD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearnCICD.Startup))]
+namespace LearnCICD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
